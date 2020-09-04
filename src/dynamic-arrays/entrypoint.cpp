09a00755c62dc06ae4847348c86d67e191a8183e
Mikhail Koshel <mouseratti@gmail.com>
2020-09-04 15:37:23 +0300

    added some strings

--- src/dynamic-arrays/entrypoint.cpp
+++ src/dynamic-arrays/entrypoint.cpp
@@ -1,28 +1,49 @@
 //
 // Created by mkoshel on 8/31/20.
 //
-# include <iostream>
+//int main() {
+//    int arraysCount, queriesCount;
+//    std::cin >> arraysCount >> queriesCount;
+//    auto arrayOfArrays = new int *[arraysCount];
+//    for (auto arrayNumber = 0; arrayNumber < arraysCount; arrayNumber++) {
+//        int arraySize;
+//        std::cin >> arraySize;
+//        int *array = new int[arraySize];
+//        arrayOfArrays[arrayNumber] = array;
+//        for (auto arrayElementIndex = 0; arrayElementIndex < arraySize; arrayElementIndex++) {
+//            std::cin >> array[arrayElementIndex];
+//        }
+//    }
+//    for (auto queryNumber = 0; queryNumber < queriesCount; queryNumber++) {
+//        int queryArrayNumber, queryArrayElementIndex;
+//        std::cin >> queryArrayNumber >> queryArrayElementIndex;
+//        std::cout << arrayOfArrays[queryArrayNumber][queryArrayElementIndex] << std::endl;
+//    }
+//    for (auto arrayNumber = 0; arrayNumber < arraysCount; arrayNumber++) {
+//        delete[] arrayOfArrays[arrayNumber];
+//    }
+//    delete[] arrayOfArrays;
+//}
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
 
 int main() {
-    int arraysCount, queriesCount;
-    std::cin >> arraysCount >> queriesCount;
-    auto arrayOfArrays = new int *[arraysCount];
-    for (auto arrayNumber = 0; arrayNumber < arraysCount; arrayNumber++) {
-        int arraySize;
-        std::cin >> arraySize;
-        int *array = new int[arraySize];
-        arrayOfArrays[arrayNumber] = array;
-        for (auto arrayElementIndex = 0; arrayElementIndex < arraySize; arrayElementIndex++) {
-            std::cin >> array[arrayElementIndex];
-        }
+    // считайте три строчки целиком и выведите их в обратном порядке
+    auto myVector = new vector<string>;
+    auto buffer = new char[256];
+    for (auto i = 0; i < 3; i++) {
+
+        cin.getline(buffer, 256);
+        myVector->push_back(buffer);
     }
-    for (auto queryNumber = 0; queryNumber < queriesCount; queryNumber++) {
-        int queryArrayNumber, queryArrayElementIndex;
-        std::cin >> queryArrayNumber >> queryArrayElementIndex;
-        std::cout << arrayOfArrays[queryArrayNumber][queryArrayElementIndex] << std::endl;
+    cout << endl;
+    for (auto myVectorIterator = myVector->end(); myVectorIterator >= myVector->begin(); myVectorIterator--) {
+        cout << *myVectorIterator << endl;
     }
-    for (auto arrayNumber = 0; arrayNumber < arraysCount; arrayNumber++) {
-        delete[] arrayOfArrays[arrayNumber];
-    }
-    delete[] arrayOfArrays;
+    delete myVector;
+
 }
